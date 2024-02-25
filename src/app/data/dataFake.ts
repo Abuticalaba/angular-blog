5aebb088c21322597d898758e8d3319464ff6e19
Esley <esley.2011@gmail.com>
2024-02-25 18:04:37 -0300

    content criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Batman",
+        "description":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ea ad quibusdam? Id quas vel non asperiores accusamus quisquam ad nihil, doloremque excepturi voluptatem eaque esse maiores adipisci perspiciatis praesentium.",
+        "photoCover":"https://images.unsplash.com/photo-1608433034711-ef6974daf968?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        "id":"2",
+        "title":"Nova série anunciada no Disney +",
+        "description":"ola",
+        "photoCover":"https://images.unsplash.com/photo-1626278664285-f796b9ee7806?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
